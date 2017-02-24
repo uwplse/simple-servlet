4db1f67e4ce5fad22ee5d8a764da4b848ad6acb5
John Toman <jtoman@cs.washington.edu>
2017-02-23 16:41:55 -0800

    Adds the pseudo attribute classes
    These can be used to transparently support global request and
    session attributes

---
+++ src/edu/washington/cse/servlet/RequestAttributes.java
@@ -0,0 +1,8 @@
+package edu.washington.cse.servlet;
+
+public class RequestAttributes {
+	public static final RequestAttributes $INSTANCE = null;
+
+	public void setAttribute(final String v, final Object value) { }
+	public Object getAttribute(final String v) { return null; }
+}
---
+++ src/edu/washington/cse/servlet/SessionAttributes.java
@@ -0,0 +1,8 @@
+package edu.washington.cse.servlet;
+
+public class SessionAttributes {
+	public static final SessionAttributes $INSTANCE = null;
+
+	public void setAttribute(final String v, final Object value) { }
+	public Object getAttribute(final String v) { return null; }
+}
