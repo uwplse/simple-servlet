7ea9dc1ce4b0ed66dc3282586c8f2d117dd669fb
John Toman <jtoman@cs.washington.edu>
2017-06-30 16:28:09 -0700

    Adds instantiation script

---
+++ scripts/instantiate.py
@@ -0,0 +1,29 @@
+import os, os.path, sys, yaml
+
+script_dir = os.path.dirname(sys.argv[0])
+
+compile_target = os.path.realpath(os.path.join(script_dir, "compile_servlet_model.py"))
+
+with open(os.path.join(script_dir, "servlet_def.py"), "r") as f:
+    blob = f.read()
+
+with open(sys.argv[1], 'r') as f:
+    global_mapping = yaml.load(f)
+
+blob += "\n"
+to_append = ["declare_globals(**{"]
+for (k,v) in global_mapping.iteritems():
+    to_append.append("    " + repr(k) + ": " + repr(v) + ",")
+to_append.append("})")
+
+blob += "\n".join([ "    " + s for s in to_append ])
+
+target_dir = sys.argv[2]
+
+if not os.path.exists(target_dir):
+    os.makedirs(target_dir)
+
+with open(os.path.join(target_dir, "servlet_def.py"), "w") as f:
+    print >> f, blob
+
+os.symlink(compile_target, os.path.join(target_dir, "compile_servlet_model.py"))
