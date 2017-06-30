1c30ebcb90008ac1e726f33aa70c7899fe0049f0
John Toman <jtoman@cs.washington.edu>
2017-06-30 16:39:05 -0700

    Make symlink relative

--- scripts/instantiate.py
+++ scripts/instantiate.py
@@ -1,8 +1,6 @@
 import os, os.path, sys, yaml
 
-script_dir = os.path.dirname(sys.argv[0])
-
-compile_target = os.path.realpath(os.path.join(script_dir, "compile_servlet_model.py"))
+script_dir = os.path.realpath(os.path.dirname(sys.argv[0]))
 
 with open(os.path.join(script_dir, "servlet_def.py"), "r") as f:
     blob = f.read()
@@ -18,7 +16,7 @@ to_append.append("})")
 
 blob += "\n".join([ "    " + s for s in to_append ])
 
-target_dir = sys.argv[2]
+target_dir = os.path.realpath(sys.argv[2])
 
 if not os.path.exists(target_dir):
     os.makedirs(target_dir)
@@ -26,4 +24,6 @@ if not os.path.exists(target_dir):
 with open(os.path.join(target_dir, "servlet_def.py"), "w") as f:
     print >> f, blob
 
-os.symlink(compile_target, os.path.join(target_dir, "compile_servlet_model.py"))
+rel_path = os.path.relpath(script_dir, target_dir)
+
+os.symlink(os.path.join(rel_path, "compile_servlet_model.py"), os.path.join(target_dir, "compile_servlet_model.py"))
